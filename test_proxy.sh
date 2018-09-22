59b751af9a4583706a04e3d7bc27246ce7ee0090
Chris Boddy <chris@boddy.im>
2018-09-22 21:14:19 +0100

    [proxy_over_p2p] tidied up integration-test script

--- test_proxy.sh
+++ test_proxy.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 #
-# serve the thing
+# clean up all the things started in this script
+#
+function teardown() {
+    jobs -p | xargs kill -9 ; 
+}
+trap teardown INT EXIT
+
+#
+# serve the thing over HTTP
 #
 SERVE_PATH=$(mktemp -d)
 echo "YOU ARE THE CHAMPION MY FRIEND" > $SERVE_PATH/index.txt
 cd $SERVE_PATH
 # serve this on port 8000
 python -m SimpleHTTPServer 8000 & 
+
 
 cd -
 
@@ -20,6 +29,10 @@ RECEIVER_LOG=$PATH1/log.log
 SENDER_LOG=$PATH2/log.log
 
 export IPFS_PATH=$PATH1
+
+#
+# start RECEIVER IPFS daemon 
+#
 $IPFS init >> $RECEIVER_LOG 2>&1
 $IPFS config --json Experimental.Libp2pStreamMounting true >> $RECEIVER_LOG 2>&1
 $IPFS config --json Addresses.API "\"/ip4/127.0.0.1/tcp/6001\"" >> $RECEIVER_LOG 2>&1
@@ -27,8 +40,12 @@ $IPFS config --json Addresses.Gateway "\"/ip4/127.0.0.1/tcp/8081\"" >> $RECEIVER
 $IPFS config --json Addresses.Swarm "[\"/ip4/0.0.0.0/tcp/7001\", \"/ip6/::/tcp/7001\"]" >> $RECEIVER_LOG 2>&1
 $IPFS daemon >> $RECEIVER_LOG 2>&1 &
 # wait for daemon to start.. maybe?
-# ipfs id returns empty string ifwe don't wait here..
+# ipfs id returns empty string if we don't wait here..
 sleep 5 
+
+#
+# start a p2p listener on RECIVER to the HTTP server with our content
+#
 $IPFS p2p listener open test /ip4/127.0.0.1/tcp/8000 >> $RECEIVER_LOG 2>&1
 FIRST_ID=$($IPFS id -f "<id>")
 
@@ -41,7 +58,11 @@ sleep 5
 
 
 
-# request to SENDER that should go via proxy 
+# send a http request to SENDER via proxy to RECIEVER that will proxy to web-server
+
+echo "******************" 
+echo proxy response
+echo "******************" 
 curl http://localhost:5001/proxy/http/$FIRST_ID/test/index.txt
 
 

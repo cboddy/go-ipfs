7537034ba0320102cd17366c83e8c731b2904e3e
Chris Boddy <chris@boddy.im>
2018-09-23 15:09:15 +0100

    [proxy_over_p2p] tidy up

--- core/corehttp/proxy.go
+++ core/corehttp/proxy.go
@@ -7,65 +7,48 @@ import (
 	"net/http"
 	"strings"
 
-	//version "github.com/ipfs/go-ipfs"
 	core "github.com/ipfs/go-ipfs/core"
-	p2p "github.com/ipfs/go-ipfs/p2p"
 	peer "gx/ipfs/QmcZSzKEM5yDfpZbeEEZaVmaZ1zXm6JWTbrQZSB8hCVPzk/go-libp2p-peer"
-	//coreapi "github.com/ipfs/go-ipfs/core/coreapi"
-	//id "gx/ipfs/QmUDzeFgYrRmHL2hUB6NZmqcBVQtUzETwmFRUc9onfSSHr/go-libp2p/p2p/protocol/identify"
 )
 
 func ProxyOption() ServeOption {
 	return func(ipfsNode *core.IpfsNode, _ net.Listener, mux *http.ServeMux) (*http.ServeMux, error) {
 		mux.HandleFunc("/proxy/", func(w http.ResponseWriter, request *http.Request) {
-			//get free tcp port
-			_p2p := ipfsNode.P2P
-
 			// parse request
 			parsedRequest, err := parseRequest(request)
 			if err != nil {
-				// TODO: send error response
-				fmt.Println(err)
+				handleError(w, "Failed to parse request", err, 400)
 				return
 			}
-			fmt.Println("parsed proxy req")
-			fmt.Println(parsedRequest)
 
-			stream, err := _p2p.NewStreamTo(ipfsNode.Context(), parsedRequest.target, "/p2p/"+parsedRequest.name)
-			fmt.Printf("OPENED STREAM %s %s %s\n", parsedRequest.target, parsedRequest.name, stream)
+			// open connect to peer
+			stream, err := ipfsNode.P2P.NewStreamTo(ipfsNode.Context(), parsedRequest.target, "/p2p/"+parsedRequest.name)
 			if err != nil {
-				fmt.Println("OMG")
-				fmt.Println(err)
+				msg := fmt.Sprintf("Failed to open stream '%v' to target peer '%v'", parsedRequest.name, parsedRequest.target)
+				handleError(w, msg, err, 500)
 				return
 			}
-			// serialize proxy request
+
+			// send request to peer
 			proxyReq, err := http.NewRequest(request.Method, parsedRequest.httpPath, request.Body)
 
 			if err != nil {
-				fmt.Println(err)
-				// TODO: send error response
+				handleError(w, "Failed to format proxy request", err, 500)
 				return
 			}
-			// send request to proxy target
 
-			fmt.Printf("OPENED READER TO STREAM %v\n", *proxyReq)
 			proxyReq.Write(stream)
-			fmt.Printf("FINISHED WRITING PROXY REQEUST\n")
 
 			s := bufio.NewReader(stream)
 			proxyResponse, err := http.ReadResponse(s, proxyReq)
 			defer func() { proxyResponse.Body.Close() }()
-			fmt.Printf("GOT PROXY RESPONSE %v\n", *proxyResponse)
 			if err != nil {
-				// TODO: send error response
-				fmt.Println(err)
+				msg := fmt.Sprintf("Failed to send request to stream '%v' to peer '%v'", parsedRequest.name, parsedRequest.target)
+				handleError(w, msg, err, 500)
 				return
 			}
 			// send client response
 			proxyResponse.Write(w)
-
-			fmt.Println("Finished!")
-
 		})
 		return mux, nil
 	}
@@ -77,7 +60,7 @@ type proxyRequest struct {
 	httpPath string // path to send to the proxy-host
 }
 
-//from the url path parse the peer-ID, name and http path
+// from the url path parse the peer-ID, name and http path
 // /http/$peer_id/$name/$http_path
 func parseRequest(request *http.Request) (*proxyRequest, error) {
 	path := request.URL.Path
@@ -100,14 +83,9 @@ func parseRequest(request *http.Request) (*proxyRequest, error) {
 	return &proxyRequest{peerID, split[4], split[5]}, nil
 }
 
-// get stream from p2p registry or nil if not present
-func getStreamForPeer(ipfsNode *core.IpfsNode, peerID *peer.ID) *p2p.StreamInfo {
-	for _, stream := range ipfsNode.P2P.Streams.Streams {
-		//TODO: is this comparing?
-		fmt.Printf("Hey! found peer %s", stream.RemotePeer)
-		if stream.RemotePeer == *peerID {
-			return stream
-		}
-	}
-	return nil
+// log error and send response to client
+func handleError(w http.ResponseWriter, msg string, err error, code int) {
+	w.WriteHeader(code)
+	fmt.Fprintf(w, "%s: %s\n", msg, err)
+	log.Warningf("server error: %s: %s", err)
 }
